323894e5e92353633742383a7be15f8c7b1f09ac
hito0559 <hitoyamaitachi@gmail.com>
2022-06-18 22:13:13 +0300

    initial commit

---
+++ lib/core/constant/strings.dart
@@ -0,0 +1,11 @@
+// App
+const String appTitle = 'EShop';
+
+// Networking and APIs
+const String baseUrl = 'https://e-commerce-mock-api-webservice.onrender.com';
+const String defaultApiKey = '';
+const String defaultSources = '';
+
+// Storage and Databases
+const String articlesTableName = '';
+const String databaseName = '';
